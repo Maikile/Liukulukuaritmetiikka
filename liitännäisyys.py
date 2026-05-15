1288d5c572226ff051b36d8063aa28780b61db70
Maikile <156587891+Maikile@users.noreply.github.com>
2026-05-15 03:15:42 +0300

    Update liitännäisyys.py

--- liitännäisyys.py
+++ liitännäisyys.py
@@ -4,8 +4,8 @@ a = np.float32(2**25)
 b = np.float32(-(2**25))
 c = np.float32(1.0)
 
-l1 = a+(b+c)
-l2 = (a+b)+c
+l1 = a + (b + c)
+l2 = (a + b) + c
 
-print(l1)
-print(l2)
+print(f"a + (b + c) = {l1}")
+print(f"(a + b) + c = {l2}")
